fdc22abe818bba868a6b0e4a5a0df3185a995ca8
wakchaureomkar <wakchaureomkar9@gmail.com>
2026-07-17 09:41:02 +0530

    Initial commit: Spring Boot project setup

---
+++ worksphere/src/main/java/com/worksphere/worksphere/WorksphereApplication.java
@@ -0,0 +1,13 @@
+package com.worksphere.worksphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorksphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorksphereApplication.class, args);
+	}
+
+}
---
+++ worksphere/src/test/java/com/worksphere/worksphere/WorksphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worksphere.worksphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorksphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
